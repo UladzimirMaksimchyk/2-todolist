cd4c714df27ce01b6a758d2df1bd7fb78d1179a3
Uladzimir <boba70325@gmail.com>
2024-05-31 15:51:27 +0300

    Update "Filter.tsx"

---
+++ src/filter/Filter.tsx
@@ -0,0 +1,63 @@
+
+import React, { useState } from "react";
+import "../App.css";
+
+
+
+function App_3() {
+
+    const [money, setMoney] = useState([
+        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+    ])
+
+    const[filter,setFilter]=useState("ALL")
+
+    let currentMoney = money;
+
+    if(nameButton==="Dollars"){
+        currentMoney=money.filter(filteredMoney=>filteredMoney.banknots === "Dollars");
+    }
+
+    if(nameButton==="RUBLS"){
+        currentMoney=money.filter(filteredMoney=>filteredMoney.banknots === "RUBLS");
+    }
+
+
+    const onClickFilterHandler = (nameButton: string) => {
+        console.log(nameButton)
+    }
+
+
+    return (
+        <>
+            <ul>
+                {currentMoney.map((objFromMoneyArr, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objFromMoneyArr.banknots}</span>
+                            <span> {objFromMoneyArr.value}</span>
+                            <span>{objFromMoneyArr.number}</span>
+
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{ margin: "45px" }}>
+                <button onClick={() => onClickFilterHandler("ALL")}>All</button>
+                <button onClick={() => onClickFilterHandler("RUBLS")}>RUBLS</button>
+                <button onClick={() => onClickFilterHandler("Dollars")}>Dollars</button>
+
+            </div>
+
+        </>
+    );
+}
+
+export default App_3;
