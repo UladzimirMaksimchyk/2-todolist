764dff54d03cd9197297d51982052791548c4d34
Uladzimir <boba70325@gmail.com>
2024-06-05 16:01:09 +0300

    Added button "x"

---
+++ src/Lessons01_2/DeleteTasks_2.tsx
@@ -0,0 +1,38 @@
+import { Button } from "../Button";
+import { TaskType } from "./DeleteTasks";
+
+type PropsType = {
+	title: string
+	tasks: TaskType[]
+}
+
+export const Todolist_1 = ({title, tasks}: PropsType) => {
+
+	return (
+		<div>
+			<h3>{title}</h3>
+			<div>
+				<input/>
+				<Button title={'+'}/>
+			</div>
+			{
+				tasks.length === 0
+					? <p>Тасок нет</p>
+					: <ul>
+						{tasks.map((task) => {
+							return <li key={task.id}>
+								   <input type="checkbox" checked={task.isDone}/>
+								   <span>{task.title}</span>
+								   <button>x</button>
+								   </li>
+						})}
+					</ul>
+			}
+			<div>
+				<Button title={'All'}/>
+				<Button title={'Active'}/>
+				<Button title={'Completed'}/>
+			</div>
+		</div>
+	)
+}
