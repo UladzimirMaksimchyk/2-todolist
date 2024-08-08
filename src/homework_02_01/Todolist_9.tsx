04b5d62553435a0c6225cd992da969da12bd789f
Uladzimir <boba70325@gmail.com>
2024-08-08 19:45:45 +0300

    Update "PropsType" to the "Todolist_9.tsx"

--- src/homework_02_01/Todolist_9.tsx
+++ src/homework_02_01/Todolist_9.tsx
@@ -11,7 +11,7 @@ type PropsType = {
     todolistId:string
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskId: string) => void
+    removeTask: (todolistId:string,taskId: string) => void
     changeFilter: (todolistId:string,value: FilterValuesType) => void
     addTask: (title: string) => void
     changeTaskStatus: (taskId: string, isDone: boolean) => void
@@ -62,7 +62,7 @@ export function Todolist_9(props: PropsType) {
         <ul>
             {
                 props.tasks.map(t => {
-                    const onClickHandler = () => props.removeTask(t.id)
+                    const onClickHandler = () => props.removeTask(props.todolistId,t.id)
                     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
                         props.changeTaskStatus(t.id, e.currentTarget.checked);
                     }
